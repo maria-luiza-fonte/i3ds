b39881de84ae8115e8a5741af59e4ad973018a11
Maria <marialuizafonte946@gmail.com>
2026-02-12 11:29:44 -0300

    Remover estilos CSS desnecessários e atualizar estrutura do componente App para incluir o componente Perfil

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Perfil from './componts/Perfil/Perfil'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='App'>
+      <div className='perfil'>
+
+<Perfil/>
+
+      <img src="https://placehold.co/200x200" alt=""/>
+      <p>@Seu Nome</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='switch'>
+        botão switch
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className='links'></div>
+      <div className='socialLinks'></div>
+      <div className='rodape'></div>
+    </div>
+  </>
   )
 }
 
