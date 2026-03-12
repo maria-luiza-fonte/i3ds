0ae693bc391b45704d47d941b806458ac1cd3ecd
Maria <marialuizafonte946@gmail.com>
2026-03-12 14:25:52 -0300

    feat: adicionar roteamento com react-router e criar páginas Home e Sobre

---
+++ 05-react-router/src/componentes/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav className='navbar'>
+      <div className='container'>
+<Link to="/">Home</Link>
+<Link to="/sobre">Sobre</Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
